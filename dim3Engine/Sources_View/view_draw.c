8ac6f2f75b9c887ecd2f9cf5f53fb364d84b7ddc
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2008-07-11 20:39:39 +0000

    07_11_2008 PC
    git-svn-id: http://dim3.googlecode.com/svn/trunk@194 2c4a6428-2a2a-0410-ad13-7dde452bfe2e

--- dim3Engine/Sources_View/view_draw.c
+++ dim3Engine/Sources_View/view_draw.c
@@ -75,21 +75,229 @@ extern void portal_compile_gl_lists(int tick,int rn);
 
 
 
+unsigned char *pixels=NULL;
+
+
+// 1. SORT the meshes
+// 2. combine stenciling across 1..255
+
+
+
+int test_me_sort(int max_mesh,int *mesh_sort_list)
+{
+	int					n,k,t,sz,d,cnt,idx;
+	int					*sptr,*dist;
+	map_mesh_type		*mesh;
+
+
+	dist=valloc(max_mesh*sizeof(int));
+
+	cnt=0;
+
+	sptr=mesh_sort_list;
+
+	for (n=0;n!=map.nportal;n++) {
+
+		for (k=0;k!=map.portals[n].mesh.nmesh;k++) {
+
+			mesh=&map.portals[n].mesh.meshes[k];
+
+			d=distance_get(mesh->box.mid.x,mesh->box.mid.y,mesh->box.mid.z,view.camera.pos.x,view.camera.pos.y,view.camera.pos.z);
+		
+			idx=-1;
+		
+			for (t=0;t!=cnt;t++) {
+				if (dist[t]>d) {
+					idx=t;
+					break;
+				}
+			}
+		
+				// insert at end of list
+				
+			if (idx==-1) {
+				dist[cnt]=d;
+				mesh_sort_list[cnt]=(n*1000)+k;
+				cnt++;
+				continue;
+			}
+			
+				// insert in list
+				
+			sz=sizeof(int)*(cnt-idx);
+			memmove(&dist[idx+1],&dist[idx],sz);
+			memmove(&mesh_sort_list[idx+1],&mesh_sort_list[idx],sz);
+			
+			dist[idx]=d;
+			mesh_sort_list[idx]=(n*1000)+k;
+			
+			cnt++;
+		}
+	}
+
+	free(dist);
+
+	return(cnt);
+}
 
 
 // supergumba -- testing out mesh elimination algorithms
 void test_me(int nportal,int *portal_list)
 {
-/*
-	int		n,k,pcnt,mcnt,dcnt,dist;
-	bool	hit;
-	map_mesh_type	*mesh;
+	int					n,k,t,stencil_idx,stencil_mesh_start_idx,idx,max_mesh,mesh_cnt,portal_idx,mesh_idx,
+						portal_cnt,hide_cnt,depth_cnt,dist;
+	int					*mesh_sort_list,*mesh_hit_list;
+	bool				hit;
+	unsigned char		*sp;
+	d3pnt				*pt;
+	map_mesh_type		*mesh;
+	map_mesh_poly_type	*poly;
+	texture_type		*texture;
 
-	pcnt=mcnt=dcnt=0;
+	if (pixels==NULL) pixels=valloc(800*600);
+
+		// get sort array for meshes
+
+	max_mesh=0;
+
+	for (n=0;n!=map.nportal;n++) {
+		max_mesh+=map.portals[n].mesh.nmesh;
+	}
+
+	mesh_sort_list=valloc(sizeof(int)*max_mesh);
+
+	mesh_cnt=test_me_sort(max_mesh,mesh_sort_list);
+
+	mesh_hit_list=valloc(sizeof(int)*mesh_cnt);
+	bzero(mesh_hit_list,(sizeof(int)*mesh_cnt));
+
+		// attempt to check out meshes that draw
+
+	depth_cnt=0;
+
+	gl_setup_viewport(console_y_offset());
+	gl_3D_view(&view.camera);
+	gl_3D_rotate(&view.camera.ang);
+	gl_setup_project();
+
+	gl_texture_bind_start();
+
+	glDisable(GL_BLEND);
+
+	glEnable(GL_STENCIL_TEST);
+	glStencilOp(GL_KEEP,GL_KEEP,GL_REPLACE);
+
+	glClear(GL_STENCIL_BUFFER_BIT);
+
+	glDisable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_NOTEQUAL,0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDepthMask(GL_TRUE);
+
+	glColorMask(GL_FALSE,GL_FALSE,GL_FALSE,GL_FALSE);
+
+		// use multiple stencils so
+		// we don't have to constantly read from stencil buffer
+
+	stencil_idx=1;
+	stencil_mesh_start_idx=0;
+
+		// run through meshes
+
+	gl_texture_opaque_start();
+
+	for (n=0;n!=mesh_cnt;n++) {
+
+		portal_idx=mesh_sort_list[n]/1000;
+		mesh_idx=mesh_sort_list[n]%1000;
+
+		mesh=&map.portals[portal_idx].mesh.meshes[mesh_idx];
+		
+			// ignore all moving meshes as they
+			// won't always obscure
+
+		if (mesh->flag.moveable) continue;
+
+			// draw polygons and use the stencil
+			// buffer to detect z-buffer changes
+
+		glStencilFunc(GL_ALWAYS,stencil_idx,0xFF);
+
+		hit=FALSE;
+
+		for (t=0;t!=mesh->npoly;t++) {
+
+			poly=&mesh->polys[t];
+
+				// ignore all transparent polygons
+
+			texture=&map.textures[poly->txt_idx];
+			if ((texture->bitmaps[0].alpha_mode==alpha_mode_transparent) || (poly->alpha!=1.0f)) continue;
+
+				// write to the stencil buffer if any part
+				// of the polygon can be seen
+
+			gl_texture_opaque_set(texture->bitmaps[0].gl_id);
+
+			glBegin(GL_POLYGON);
+
+			for (k=0;k!=poly->ptsz;k++) {
+				pt=&mesh->vertexes[poly->v[k]];
+				glTexCoord2f(poly->gx[k],poly->gy[k]);
+				glVertex3i((pt->x-view.camera.pos.x),(pt->y-view.camera.pos.y),(view.camera.pos.z-pt->z));
+			}
+
+			glEnd();
+		}
+
+			// read the stencil to look for hits
+
+		if ((stencil_idx==255) || ((n+1)==mesh_cnt)) {
+
+			glReadPixels(0,0,setup.screen.x_sz,setup.screen.y_sz,GL_STENCIL_INDEX,GL_UNSIGNED_BYTE,pixels);
+
+			sp=pixels;
+
+			for (k=0;k!=(800*600);k++) {
+				
+				idx=*sp++;
+
+				if (idx!=0) {
+					idx+=stencil_mesh_start_idx;
+					if (mesh_hit_list[idx]==0x0) {
+						depth_cnt++;
+						mesh_hit_list[idx]=0x1;
+					}
+				}
+			}
+			
+			stencil_idx=1;
+			stencil_mesh_start_idx=n+1;
+
+			if ((n+1)!=mesh_cnt) glClear(GL_STENCIL_BUFFER_BIT);
+		}
+		else {
+			stencil_idx++;
+		} 
+	}
+
+	gl_texture_opaque_end();
+
+	glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE);
+	glClear(GL_STENCIL_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_STENCIL_TEST);
+
+	free(mesh_sort_list);
+
+	portal_cnt=0;
 
 	for (n=0;n!=nportal;n++) {
-		pcnt+=map.portals[portal_list[n]].mesh.nmesh;
+		portal_cnt+=map.portals[portal_list[n]].mesh.nmesh;
 	}
+
+	hide_cnt=0;
 
 	for (n=0;n!=map.nportal;n++) {
 
@@ -99,23 +307,16 @@ void test_me(int nportal,int *portal_list)
 
 
 			dist=distance_get(mesh->box.mid.x,mesh->box.mid.y,mesh->box.mid.z,view.camera.pos.x,view.camera.pos.y,view.camera.pos.z);
-			hit=boundbox_inview(mesh->box.min.x,mesh->box.min.z,mesh->box.max.x,mesh->box.max.z,mesh->box.min.y,mesh->box.max.y);
+			if (dist<(view.camera.far_z-view.camera.near_z)) {
 
-			if (hit) {
-
-			fprintf(stdout,"%d -> %d\n",dist,(view.camera.far_z-view.camera.near_z));
-				if (dist<(view.camera.far_z-view.camera.near_z)) {
-					dcnt++;
-				}
-				mcnt++;
+				if (boundbox_inview(mesh->box.min.x,mesh->box.min.z,mesh->box.max.x,mesh->box.max.z,mesh->box.min.y,mesh->box.max.y)) hide_cnt++;
 			}
 
 			mesh++;
 		}
 	}
 
-	fprintf(stdout,"portal/mesh/mesh+dist %d/%d/%d\n",pcnt,mcnt,dcnt);
-*/
+	fprintf(stdout,"max/portal/view hide/zbuffer %d/%d/%d/%d\n",max_mesh,portal_cnt,hide_cnt,depth_cnt);
 }
 
 
